cb870f48beaa118f6bf4559954a5f2c7f0dca076
baesisi3648 <baesisi3648@gmail.com>
2026-01-12 00:37:42 +0900

    Title: 두 개의 격자 비교하기, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260112/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,22 @@
+n, m = map(int,input().split())
+
+arr_1=[]
+arr_2=[]
+
+for _ in range(m):
+    raw_data = list(map(int, input().split()))
+    arr_1.append(raw_data)
+
+for _ in range(m):
+    raw_data = list(map(int, input().split()))
+    arr_2.append(raw_data)
+
+for i in range(n):
+    for j in range(m):
+        a = 0
+        if arr_1[i][j] == arr_2[i][j]:
+            a = 0
+        else:
+            a = 1
+        print(a,end=' ')
+    print()
